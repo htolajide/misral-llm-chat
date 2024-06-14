3a187c5f8078467b4271e429bc6088c53d8acb78
Hammed Taofeek <htolajide@yahoo.com>
2024-06-15 01:19:56 +0200

    mistral llm resset

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 
 api_key = os.getenv("MISTRAL_API_KEY")
-""""Note you can get the MISTRAL API from the official website to subscribing at
-https://auth.mistral.ai/ui/login?flow=94362866-2bfa-42ea-b73f-f9227693422b
-also its not allowed to expose the the KEY save it in environment variable b
-y using the following command
-    echo 'export MISTRAL_API_KEY="your_openai_api_key"' >> ~/.bashrc
-    source ~/.bashrc
-    then get it the source code by using
-    api_key = os.getenv("MISTRAL_API_KEY")
-"""
+#""""Note you can get the MISTRAL API from the official website to subscribing at
+#https://auth.mistral.ai/ui/login?flow=94362866-2bfa-42ea-b73f-f9227693422b
+#also its not allowed to expose the the KEY save it in environment variable b
+#y using the following command
+#   echo 'export MISTRAL_API_KEY="your_openai_api_key"' >> ~/.bashrc
+#  source ~/.bashrc
+#   then get it the source code by using
+#   api_key = os.getenv("MISTRAL_API_KEY")
+#"""
 model = "mistral-large-latest"
 
 client = MistralClient(api_key=api_key)
